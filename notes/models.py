2595b3c8f171ddc28353f946bf8ff7206abb4f7a
Alex Chernyshev <graphs.alex@gmail.com>
2024-07-16 18:11:12 -0400

    add indexing

--- notes/models.py
+++ notes/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Note(models.Model):
     """Note model."""
 
-    title = models.CharField(max_length=250)
-    body = models.TextField(blank=True)
+    title = models.CharField(max_length=250, db_index=True)
+    body = models.TextField(blank=True, db_index=True)
     owner = models.ForeignKey(User, related_name='notes', on_delete=models.CASCADE, null=False)
     shared_with = models.ManyToManyField(User, related_name='shared_notes', blank=True)
     created = models.DateTimeField(auto_now_add=True, null=False)
