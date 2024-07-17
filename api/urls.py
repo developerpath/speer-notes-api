81d86a8d7a3e10af6e7a92d36b792f921cff2b98
Alex Chernyshev <graphs.alex@gmail.com>
2024-07-16 23:43:50 -0400

    cleanup

--- api/urls.py
+++ api/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
 from notes.views import SearchNotesView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/search/', SearchNotesView.as_view(), name='search_notes'),
     path('api/notes/', include('notes.urls')),
