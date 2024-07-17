c2e5d4b5c5e349735d117060d8ce0439bd82201f
Alex Chernyshev <graphs.alex@gmail.com>
2024-07-16 22:18:47 -0400

    update auth serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,14 +1,34 @@
 from rest_framework import serializers
 from .models import User
+import re
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(min_length=5)
     password = serializers.CharField(
+        min_length=8,
+        max_length=16,
         style={'input_type': 'password'}
     )
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("A user with that username already exists.")
+        return value
+
+    def validate_password(self, value):
+        if not re.search(r'[a-z]', value):
+            raise serializers.ValidationError("Password must contain at least one lowercase letter.")
+        if not re.search(r'[A-Z]', value):
+            raise serializers.ValidationError("Password must contain at least one uppercase letter.")
+        if not re.search(r'[0-9]', value):
+            raise serializers.ValidationError("Password must contain at least one number.")
+        if not re.search(r'[\W_]', value):
+            raise serializers.ValidationError("Password must contain at least one special character.")
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
